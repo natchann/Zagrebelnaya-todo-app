41b066b05d122e29a7f27e997103a0a205415295
Nastya <hohlushka.zag@gmail.com>
2019-06-29 23:51:29 +0300

    classes

---
+++ main.js
@@ -0,0 +1,36 @@
+class toDoApp {
+    constructor (){
+
+    }
+    excute(){
+
+    }
+
+}
+
+class Task {
+    constructor(title="Заголовок"){
+        this.title + title;
+        
+    }
+    isDone(isDone){
+        this.isDone = isDone;
+    }
+}
+
+class TaskManager{
+    constructor(){
+
+    }
+    add(){
+
+    }
+    remove(){
+
+    }
+
+}
+
+class {
+    
+}
